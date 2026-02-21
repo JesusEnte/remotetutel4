945eb851735929272cafa8e5ad622996f1fd9f95
JesusEnte <bock-david@outlook.de>
2026-02-21 18:49:34 +0100

    make sure turtles have a persistent id

--- turtle/startup_template.lua
+++ turtle/startup_template.lua
@@ -1,5 +1,6 @@
 SERVER_URL = '' --empty cuz template
 
+if not os.getComputerLabel() then os.setComputerLabel('tutel #' .. os.getComputerID()) end
 shell.run(string.format('wget run %sremotetutel.lua', SERVER_URL))
 print('Failed to fetch program from server, rebooting in 20 seconds.')
 sleep(20)
